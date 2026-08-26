74a845b24f66c7d6dfb23444c02a7e067065cde4
Massiwayne Chikirou <massiwayne.chikirou@sonarsource.com>
2026-08-26 14:07:03 +0200

    Add intentional CI failure fixture

--- orders_test.go
+++ orders_test.go
@@ -1,3 +1,9 @@
 package orders
 import "testing"
 func TestTotal(t *testing.T) { if Total([]int{2, 3}) != 5 { t.Fatal("unexpected total") } }
+
+func TestIntentionalFailure(t *testing.T) {
+	if Total([]int{2, 3}) != 999 {
+		t.Fatal("intentional failure for CI testing")
+	}
+}
